b763ef3e9bf6052beec7a9cc5cbeae287de7b774
Aditya Gupta <adityaguptamorena@gmail.com>
2025-07-14 21:25:46 +0530

    added 7 more

--- Array/Practice1.java
+++ Array/Practice1.java
@@ -256,7 +256,79 @@ solution: int arr[] = {1,2,3,4,5,6,7,8,9,10};
             }
         }
     System.out.println(min);  
-        
+
+31. Read the max even value from an array?
+solution: int arr[] = {1,2,3,4,5,6,7,8,9,10};
+        int max = Integer.MIN_VALUE;
+        for(int i=0;i<arr.length;i++){
+            if(arr[i] % 2 == 0){
+            if(arr[i] > max){
+            max=arr[i];
+            }
+            }
+        }
+    System.out.println(max);  
+
+32. Read the min odd value from an array?
+solution: int arr[] = {1,2,3,4,5,6,7,8,9,10};
+        int min = Integer.MAX_VALUE;
+        for(int i=0;i<arr.length;i++){
+            if(arr[i] % 2!=0){
+            if(arr[i] < min){
+            min=arr[i];
+            }
+            }
+        }
+    System.out.println(min);  
+
+33. Read the max odd value from an array?
+solution: int arr[] = {1,2,3,4,5,6,7,8,9,10};
+        int max = Integer.MIN_VALUE;
+        for(int i=0;i<arr.length;i++){
+            if(arr[i] % 2 != 0){
+            if(arr[i] > max){
+            max=arr[i];
+            }
+            }
+        }
+    System.out.println(max);
+
+34. Read middle 3 values from an array which contains odd number of elements
+solution: int arr[] = {1,2,3,4,5,6,7};
+          if(arr.length %2 != 0 &&  arr.length >= 3){
+          int mid = arr.length/2;
+            System.out.println(arr[mid-1] +" "+ arr[mid] +" "+ arr[mid+1]);
+          }else{
+            System.out.println("Array doesn't have odd number of elements or has less than 3 elements.");
+          }
+
+35. Read middle 5 values from an array which contains odd number of elements
+solution: int arr[] = {1,2,3,4,5,6,7};
+          if(arr.length %2 != 0 &&  arr.length >= 5){
+          int mid = arr.length/2;
+            System.out.println(arr[mid-2]+" "+arr[mid-1] +" "+ arr[mid] +" "+ arr[mid+1]+" "+arr[mid+2]);
+          }else{
+            System.out.println("Array doesn't have odd number of elements or has less than 5 elements.");
+          }
+
+36. Read middle 4 values from an array which contains even number of elements
+solution: int arr[] = {1,2,3,4,5,6,7,8};
+        if(arr.length %2 == 0 &&  arr.length >= 4){
+        int mid = arr.length/2;
+            System.out.println(arr[mid-2]+" "+arr[mid-1] +" "+ arr[mid] +" "+ arr[mid+1]);
+        }else{
+            System.out.println("Array doesn't have even number of elements or has less than 4 elements.");
+        }
+
+37. Read middle 6 values from an array which contains even number of elements
+solution: int arr[] = {1,2,3,4,5,6,7,8};
+        if(arr.length %2 == 0 &&  arr.length >= 6){
+        int mid = arr.length/2;
+            System.out.println(arr[mid-3]+" "+arr[mid-2]+" "+arr[mid-1] +" "+ arr[mid] +" "+ arr[mid+1]+" "+arr[mid+2]);
+        }else{
+            System.out.println("Array doesn't have even number of elements or has less than 6 elements.");
+        }
+
 */
 public class Practice1{
      
