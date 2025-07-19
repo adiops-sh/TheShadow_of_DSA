f8790cb15265e102a0775c9f6f40b52e117b921d
Aditya Gupta <adityaguptamorena@gmail.com>
2025-07-19 21:43:50 +0530

    Solve some Star Pattern

--- Patterns/starPattern.java
+++ Patterns/starPattern.java
@@ -1,27 +1,109 @@
 /*
-
 1.  *****
     *****
     *****
     *****
     *****
-
+solution: int n=5;
+        for(int i=0;i<n;i++){
+            for(int j=0;j<n;j++){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
 
 2.  *
     **
     ***
     ****
     *****
-
+solution: int n=5;
+        for(int i=1;i<=n;i++){
+            for(int j=0;j<i;j++){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
 
 3.  *****
     ****
     ***
     **
     *
+solution: int n=5;
+       for(int i=0;i<n;i++){
+            for(int j=0;j<n-i;j++){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
 
+4.       *
+        **
+       ***
+      ****
+     *****
+solution: int n=5;
+    for(int i=1;i<=n;i++){
+        for(int sps =0;sps<n-i;sps++){
+            System.out.print(" ");
+        }
+            for(int j=0;j<i;j++){
+                System.out.print("*");
+            }
+            System.out.println();
+    }    
 
-4.  *
+5.   *****
+      ****
+       ***
+        **
+         *
+solution: int n=5;
+    for(int i=1;i<=n;i++){
+        for(int sps=0;sps<i;sps++){
+            System.out.print(" ");
+        }
+            for(int j=0;j<=n-i;j++){
+                System.out.print("*");
+            }
+            System.out.println();
+    }
+
+6.      *
+       ***
+      *****
+     *******
+    *********
+solution: int n=5;
+    for(int i=1;i<=n;i++){
+        for(int sps=0;sps<n-i;sps++){
+            System.out.print(" ");
+        }
+            for(int j=1;j<=(2*i-1);j++){
+                System.out.print("*");
+            }
+            System.out.println();
+    }
+
+7.  *********
+     *******
+      *****
+       ***
+        *
+solution: for(int i = 0; i < n; i++) {
+    for(int sps = 0; sps < i; sps++) {
+        System.out.print(" ");
+    }
+
+    for(int j = 0; j < (2 * (n - i) - 1); j++) {
+        System.out.print("*");
+    }
+
+    System.out.println();
+}
+
+8.  *
     **
     ***
     ****
@@ -32,40 +114,11 @@
     *
 
 
-5.       *
-        **
-       ***
-      ****
-     *****
-
-
-6.   *****
-      ****
-       ***
-        **
-         *
-
-
-7.      *
-       ***
-      *****
-     *******
-    *********
-
-
-8.  *********
-     *******
-      *****
-       ***
-        *
-
-
-9.      *
+9.       *
         * *
        * * *
       * * * *
      * * * * *
-
 
 10.  * * * * *
       * * * *
@@ -190,5 +243,21 @@
 public class starPattern{
     public static void main(String[] args) {
         System.out.println("Happy Coding!");
+        int n=5;
+
+for(int i = 0; i < n; i++) {
+    for(int sps = 0; sps < i; sps++) {
+        System.out.print(" ");
+    }
+
+    for(int j = 0; j < (2 * (n - i) - 1); j++) {
+        System.out.print("*");
+    }
+
+    System.out.println();
+}
+
+        
+
     }
 }
