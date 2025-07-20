b83da21cee49968d7670de03ad4f4941ef62d737
Aditya Gupta <adityaguptamorena@gmail.com>
2025-07-20 10:53:09 +0530

    complete 160th problem

--- Array/day-6/ArrayLogicalQuestion.java
+++ Array/day-6/ArrayLogicalQuestion.java
@@ -106,22 +106,113 @@ solution:int arr [] = {10, 20, 30, 40, 50, 60, 70, 80, 90, 100};
             System.out.println(Arrays.toString(arr));
 
 151. Assume an array with 10 elements. right shift by one
-solution:
+solution: int arr [] = {10, 20, 30, 40, 50, 60, 70, 80, 90, 100};
+//[100, 10, 20, 30, 40, 50, 60, 70, 80, 90]
+            int lastEle = arr[arr.length-1];
+            for(int i=arr.length-2;i>=0;i--){
+                arr[i+1] = arr[i];
+            }
+                arr[0]=lastEle;
+            System.out.println(Arrays.toString(arr));
+    
 
-152. Assume an array with 10 elements. rightshift by two
-solution:
+152. Assume an array with 10 elements. right shift by two
+solution: int arr [] = {10, 20, 30, 40, 50, 60, 70, 80, 90, 100};
+            int lastEle=arr[arr.length-1];
+            int secLastEle=arr[arr.length-2];
+            for(i=arr.length-3;i>=0;i--){
+                arr[i+2]=arr[i];
+            }
+                arr[1]=lastEle;
+                arr[0]=secLastEle;
+            System.out.println(Arrays.toString(arr));
 
-153. Assume an array with 10 elements. rightshift by three
-solution:
+153. Assume an array with 10 elements. right shift by three
+solution: int arr [] = {10, 20, 30, 40, 50, 60, 70, 80, 90, 100};
+            int lastEle=arr[arr.length-1];
+            int secLastEle=arr[arr.length-2];
+            int thirdLastEle=arr[arr.length-3];
+            for(i=arr.length-4;i>=0;i--){
+                arr[i+3]=arr[i];
+            }
+                arr[2]=lastEle;
+                arr[1]=secLastEle;
+                arr[0]=thirdLastEle;
+            System.out.println(Arrays.toString(arr));
 
-154. Assume an array with 10 elements. rightshift by one only in the first half
-solution:
+154. Assume an array with 10 elements. right shift by one only in the first half
+solution: int arr [] = {10, 20, 30, 40, 50, 60, 70, 80, 90, 100};
+            int lastEle=arr[(arr.length/2)-1];
+            for(int i=arr.length/2-2;i>=0;i--){
+                arr[i+1]=arr[i];
+            }
+                arr[0]=lastEle;
+            System.out.println(Arrays.toString(arr));
 
 155. Assume an array with 10 elements. rightshift by one only in the 2nd half
-solution:
+solution:int arr [] = {10, 20, 30, 40, 50, 60, 70, 80, 90, 100};
+            int lastEle=arr[arr.length-1];
+            for(int i=arr.length-2;i>=arr.length/2;i--){
+                arr[i+1]=arr[i];
+            }
+                arr[arr.length/2]=lastEle;
+            System.out.println(Arrays.toString(arr));
 
-156. Assume an array with 10 elements. rightshift by two only in the 2nd half
-solution:
+156. Assume an array with 10 elements. right shift by two only in the 2nd half
+solution:int arr [] = {10, 20, 30, 40, 50, 60, 70, 80, 90, 100};
+            int lastEle=arr[arr.length-1]; //100
+            int secLastEle=arr[arr.length-2]; //90
+            for(int i=arr.length-3;i>=arr.length/2;i--){
+                arr[i+2]=arr[i];
+            }
+                arr[arr.length/2+1]=lastEle; //70
+                arr[arr.length/2]=secLastEle; //60
+            System.out.println(Arrays.toString(arr));
+
+157. Assume an array with 10 elements. rightshift by two only in the first half
+solution: int arr [] = {10, 20, 30, 40, 50, 60, 70, 80, 90, 100};
+            int last = arr[(arr.length/2)-1]; //50
+            int secLast = arr[(arr.length/2)-2]; //40
+            for(int i=(arr.length/2)-3; i>=0;i++){
+                arr[i+2]=arr[i];
+            }
+                arr[0]=secLast;
+                arr[1]=last;
+            System.out.println(Arrays.toString(arr));
+
+158. Assume an array with 10 elements. right rotate by one
+solution: int arr [] = {10, 20, 30, 40, 50, 60, 70, 80, 90, 100};
+            int last = arr[arr.length-1]; 
+            for(int i=arr.length-2;i>=0;i--){
+                arr[i+1]=arr[i];
+            }
+                arr[0]=last;
+            System.out.println(Arrays.toString(arr));
+
+
+159. Assume an array with 10 elements. right rotate by two
+solution: int arr [] = {10, 20, 30, 40, 50, 60, 70, 80, 90, 100};
+            int last = arr[arr.length-1]; 
+            int secLast = arr[arr.length-2]; 
+            for(int i=arr.length-3;i>=0;i--){
+                arr[i+2]=arr[i];
+            }
+                arr[0]=last;
+                arr[1]=secLast;
+            System.out.println(Arrays.toString(arr));
+
+160. Assume an array with 10 elements. right rotate by three
+solution: int arr [] = {10, 20, 30, 40, 50, 60, 70, 80, 90, 100};
+            int last = arr[arr.length-1]; 
+            int secLast = arr[arr.length-2]; 
+            int thirdLast = arr[arr.length-3]; 
+            for(int i=arr.length-4;i>=0;i--){
+                arr[i+3]=arr[i];
+            }
+                arr[0]=thirdLast;
+                arr[1]=secLast;
+                arr[2]=last;
+            System.out.println(Arrays.toString(arr));
 
  */
 
