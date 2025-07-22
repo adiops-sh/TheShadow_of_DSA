23ed79f145b61a22cf6f6fae5e2dd9bc99443903
Aditya Gupta <adityaguptamorena@gmail.com>
2025-07-22 23:52:44 +0530

    Add some Alphabet Pattern

--- Patterns/alphabetPattern.java
+++ Patterns/alphabetPattern.java
@@ -22,18 +22,40 @@
     A B C
     A B
     A
+solution: int n = 5;
+        for (int i = 0; i <n; i++) {
+            char ch = 'A';
+            for (int j = 1; j <= n-i; j++) {
+                System.out.print(ch + " ");
+                ch++;
+            }
+            System.out.println();
+        }
 
 5.  A
     B A
     C B A
     D C B A
     E D C B A
+solution: 
 
 6.     A
       A B
      A B C
     A B C D
    A B C D E
+solution:  int n = 5;
+        for (int i = 1; i <= n; i++) {
+            for (int s = 1; s <= n - i; s++) {
+                System.out.print(" ");
+            }
+            char ch = 'A';
+            for (int j = 1; j <= i; j++) {
+                System.out.print(ch + " ");
+                ch++;
+            }
+            System.out.println();
+        }
 
 7.  a
     A b
@@ -46,10 +68,37 @@
     X Y Z
     W X Y Z
     V W X Y Z
+
+9.  A 
+    A B
+    A B C
+    A B C D
+    A B C D E    
+solution:  int n = 5;
+        for(int i=0;i<n;i++){
+            char ch='A';
+            for(int j=0;j<=i;j++){
+                System.out.print(ch+" ");
+                ch++;
+            }
+            System.out.println();
+        }   
     
  */
 public class alphabetPattern {
     public static void main(String[] args) {
         System.out.println("Happy Coding!");
+        
+        int n = 5;
+        for (int i = 0; i <= n; i++) {
+            char ch = 'A';
+            for (int j = 1; j <=i; j++) {
+                System.out.print(ch + " ");
+                ch++;
+            }
+            System.out.println();
+        }
+
+
     }
 }
