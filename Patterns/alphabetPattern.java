84944c8ca4bc56180c8d326a62e14a7a9d55ce57
Aditya Gupta <adityaguptamorena@gmail.com>
2025-07-23 22:32:50 +0530

    Add more Pattern

--- Patterns/alphabetPattern.java
+++ Patterns/alphabetPattern.java
@@ -88,17 +88,6 @@ solution:  int n = 5;
 public class alphabetPattern {
     public static void main(String[] args) {
         System.out.println("Happy Coding!");
-        
-        int n = 5;
-        for (int i = 0; i <= n; i++) {
-            char ch = 'A';
-            for (int j = 1; j <=i; j++) {
-                System.out.print(ch + " ");
-                ch++;
-            }
-            System.out.println();
-        }
-
 
     }
 }
