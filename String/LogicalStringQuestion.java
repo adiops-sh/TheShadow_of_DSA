f24491a120ccb78acdd12091917898498619342b
Aditya Gupta <adityaguptamorena@gmail.com>
2025-07-28 22:47:06 +0530

    Added String based Question

---
+++ String/LogicalStringQuestion.java
@@ -0,0 +1,50 @@
+
+/*
+1. Find out the total number of characters in the given string?
+solution: String str = "Elon musk";
+            System.out.println(str.length());
+
+2. Print the first char from the given string?
+solution: String str = "Elon musk";
+            if(!str.isEmpty()){
+                System.out.println(str.charAt(0));
+            }else{
+                System.out.println("String is empty");
+            }
+
+3. Print the 2nd char from the given string?
+solution: String str = "Elon musk";
+            if((!str.isEmpty()) && (str.length()>= 2)){
+                System.out.println(str.charAt(1));
+            }else{
+                System.out.println("String is too short");
+            }
+
+4. Print the 3rd char from the given string?
+solution: String str = "Elon musk";
+            if((!str.isEmpty()) && (str.length()>= 3)){
+                System.out.println(str.charAt(2));
+            }else{
+                System.out.println("String is too short");
+            }
+
+5. Print the last char from the given string?
+solution:   String str = "Elon musk";
+            System.out.println(str.charAt(str.length()-1));
+            
+6. Print the last 2nd char from the given string?
+solution: String str = "Elon musk";
+            if (str.length() >= 2) {
+                System.out.println(str.charAt(str.length() - 2));
+            } else {
+                System.out.println("String is too short");
+            }
+
+ */
+
+public class LogicalStringQuestion {
+    public static void main(String [] args){
+        System.out.println("Happy Coding!");
+        
+    }
+}
