2ef8499fcbd77098c03d587a47f76f1f5a2667d3
Aditya Gupta <adityaguptamorena@gmail.com>
2025-08-07 22:36:23 +0530

    Question Added

--- String/LogicalStringQuestion.java
+++ String/LogicalStringQuestion.java
@@ -294,6 +294,25 @@ solution: String str = "ElonMuskTeslaInfinite";
                 System.out.print(str.charAt(i)+" ");
             }
 
+34. Write a program to count the number of vowels in a given string.
+solution: public static int countVowels(String str) {
+        int count = 0;
+
+        for (int i = 0; i < str.length(); i++) {
+            char ch = str.charAt(i);
+
+        
+            if (ch == 'a' || ch == 'e' || ch == 'i' || ch == 'o' || ch == 'u' ||
+                ch == 'A' || ch == 'E' || ch == 'I' || ch == 'O' || ch == 'U') {
+                count++;
+            }
+        }
+
+        return count;
+    }
+        String input = "Hello World";
+        int result = countVowels(input);
+        System.out.println("Number of vowels: " + result);
  */
 
 public class LogicalStringQuestion {
