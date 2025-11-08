93a49b88e7ffc6ec154a0a92e61d99e3022be4e3
Aditya Gupta <adityaguptamorena@gmail.com>
2025-11-08 18:20:45 +0530

    Added star Pattern

--- Patterns/Pattern2.java
+++ Patterns/Pattern2.java
@@ -12,5 +12,29 @@ public class Pattern2 {
             System.out.println();
             
         }
+System.out.println("====================================");
+        for (int i=1;i<=n;i++){
+            for (int j=n;j>=1;j--){
+                if(j==1 || i==n || i==j){
+                    System.out.print("* ");
+                }else{
+                    System.out.print("  ");
+                }
+            }
+            System.out.println();
+            
+        }
+System.out.println("====================================");
+        for (int i=1;i<=n;i++){
+            for (int j=1;j<=n;j++){
+                if(j==1 || i==1 || i+j == n+1){
+                    System.out.print("* ");
+                }else{
+                    System.out.print("  ");
+                }
+            }
+            System.out.println();
+            
+        }
     }
 }
