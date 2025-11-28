2a29293621f7e8a44862aa8175e97fd069f56fd8
Aditya Gupta <adityaguptamorena@gmail.com>
2025-11-28 23:21:28 +0530

    Added Insertion

---
+++ LinkedList/Aadi/InsertionInTail.java
@@ -0,0 +1,43 @@
+class InsertionInTail {
+    Node head;
+    class Node{
+        int data;
+        Node next;
+        Node(int data){
+            this.data = data;
+            this.next = null;
+        }
+    }
+
+    public void insertAtEnd(int newData){
+        Node newNode = new Node(newData);
+        if(head == null){
+            head = newNode;
+            return;
+        }
+
+        newNode.next = null;
+        Node temp = head;
+        while (temp.next != null) {
+            temp = temp.next;
+        }
+        temp.next = newNode;
+        return;
+    }
+    public void displayLL(){
+        Node current = head;
+        while (current != null) {
+            System.out.print(current.data + " ");
+            current = current.next;
+        }
+    }
+    public static void main(String[] args) {
+        InsertionInTail ll = new InsertionInTail();
+        ll.insertAtEnd(10);
+        ll.insertAtEnd(12);
+        ll.insertAtEnd(9);
+
+        ll.displayLL();  
+    }
+    
+}
